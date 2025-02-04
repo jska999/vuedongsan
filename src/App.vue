dea4e6ace361d7917a40a3fba53fa26f53911831
jska999 <jska9999@gmail.com>
2025-02-04 16:45:34 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="black-bg" v-if="is_modal_open">
+        <div class="white-bg">
+            <h4>상세페이지</h4>
+            <p>내용</p><button @click="is_modal_open=false">닫기</button>
+        </div>
+    </div>
     <div class="menu">
         <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
     </div>
     <div>
-        <h4>{{ products[0] }}</h4>
+        <img src="./assets/images/room0.jpg" class="room-img">
+        <h4 @click="is_modal_open = true">{{ products[0] }}</h4>
         <p>50만원</p>
-        <button @click="report_cnt++">허위매물신고</button>
-        <span>신고수 : {{ report_cnt }}</span>        
+        <button @click="increse(0)">허위매물신고</button>
+        <span>신고수 : {{ report_cnt[0] }}</span>        
     </div>
     <div>
+        <img src="./assets/images/room1.jpg" class="room-img">
         <h4>{{ products[1] }}</h4>
         <p>50만원</p>
-        <button>허위매물신고</button>
-        <span>신고수 : 0</span>        
+        <button @click="increse(1)">허위매물신고</button>
+        <span>신고수 : {{ report_cnt[1] }}</span>        
     </div>
     <div>
+        <img src="./assets/images/room2.jpg" class="room-img">
         <h4>{{ products[2] }}</h4>
         <p>50만원</p>
-        <button>허위매물신고</button>
-        <span>신고수 : 0</span>        
+        <button @click="increse(2)">허위매물신고</button>
+        <span>신고수 : {{ report_cnt[2] }}</span>        
     </div>
 </template>
 
@@ -29,19 +37,52 @@ export default {
     name: 'App',
     data() {
         return {
+            is_modal_open : false,
             menus: ["Home", "Products", "About"],
             products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-            report_cnt : 0,
+            report_cnt : [0,0,0],
         }
     },
     components: {
 
-    }
+    },
+    methods: {
+        increse(no) {
+            this.report_cnt[no]++;
+        }
+    },
 
 }
 </script>
 
 <style>
+body{
+    margin: 0;
+}
+
+div {
+    box-sizing: border-box;
+}
+
+.black-bg {
+    width: 100%; height: 100%;
+    background-color: rgba(0,0,0,0,5);
+    position: fixed;
+    padding: 20px;
+}
+
+.white-bg {
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
+}
+
+.room-img{
+    width: 100%;
+    margin-top: 40px;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
